a34399c9665080e839df2da3887f6f8df71b1d47
Chris Grimmett <chris@grimtech.net>
2024-01-13 03:48:29 -0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import MuxPlayer from '@mux/mux-player-react/lazy';
+
+const demoVideo = 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <p><b>Observed behavior</b></p>
+      <p>The video player does not exit fullscreen in response to the fullscreen button being pressed while in fullscreen mode.</p>
+      <p>The user is forced to use the Esc key to exit fullscreen as the mux player's fullscreen button is unresponsive.</p>
+      <br></br>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p><b>Reproduction steps.</b></p>
+      <p>* Refresh the page.</p>
+      <p>* Click play.</p>
+      <p>* While the video is buffering, enter fullscreen mode using the fullscreen button in the bottom right of the player.</p>
+      <p>* Click the fullscreen button to exit fullscreen. (nothing happens)</p>
+      <br></br>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <p><b>Alternative Reproduction steps.</b></p>
+      <p>* Refresh the page.</p>
+      <p>* Click play.</p>
+      <p>* While the video is buffering, seek to an <i>unloaded portion</i> of video. (<b>important!</b>)</p>
+      <p>* Enter fullscreen mode using the fullscreen button in the bottom right of the player.</p>
+      <p>* Click the fullscreen button. (nothing happens)</p>
+      <br></br>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <p>Reproduction steps may not work exactly the same in all environments.</p>
+      <p>I have a slow internet connection. I think the issue might be related to buffering. </p>
+      <p>Using browser tools to simulate low bandwidth connection may be helpful.</p>
+      <br></br>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <MuxPlayer
+        src={demoVideo}
+      ></MuxPlayer>
+    </div>
   )
 }
